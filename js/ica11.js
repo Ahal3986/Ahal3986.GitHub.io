0a902817fa8c9a1f5f2c79f7946ac85ad3c77584
Ahal3986 <112126365+Ahal3986@users.noreply.github.com>
2022-11-01 15:02:06 -0600

    yes

---
+++ js/ica11.js
@@ -0,0 +1,30 @@
+window.addEventListener('onload', getQuote);
+
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random'
+
+async function getQuote() {
+    console.log('test click worked');
+    let text = await fetch(endpoint);
+    let response = await text.text();
+
+    console.log(text);
+
+    let json_response = JSON.parse(response);
+    console.log(json_response);
+    console.log(json_response['message']);
+
+    displayQuote(json_response['message']);
+
+}
+
+function displayQuote(x) {
+    //const quoteBox = document.querySelector('#js-quote-text');
+    //const textMessages = document.createTextNode(x);
+    //quoteBox.innerText(textMessage);
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+getQuote();
